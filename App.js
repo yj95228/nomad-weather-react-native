a47a202fe14f33d1fdd2c44f14e70afb5cf63f4e
yj95228 <kei02119@naver.com>
2022-03-14 23:13:37 +0900

    My First App - load city from location

--- App.js
+++ App.js
@@ -1,20 +1,90 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import * as Location from 'expo-location';
+import React, { useEffect, useState } from 'react';
+import { Dimensions, ScrollView, StyleSheet, Text, View } from 'react-native';
+
+const { width: SCREEN_WIDTH } = Dimensions.get('window');
+// const SCREEN_WIDTH = Dimensions.get('window').width;
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+	const [city, setCity] = useState('Loading...');
+	const [location, setLocation] = useState();
+	const [ok, setOk] = useState(true);
+	const ask = async () => {
+		const { granted } = await Location.requestForegroundPermissionsAsync();
+		if (!granted) {
+			setOk(false);
+		}
+		const {
+			coords: { latitude, longitude },
+		} = await Location.getCurrentPositionAsync({ accuracy: 5 });
+		const location = await Location.reverseGeocodeAsync(
+			{ latitude, longitude },
+			{ useGoogleMaps: false }
+		);
+		setCity(location[0].region);
+	};
+	useEffect(() => {
+		ask();
+	}, []);
+	return (
+		<View style={styles.container}>
+			<View style={styles.city}>
+				<Text style={styles.cityName}>{city}</Text>
+			</View>
+			<ScrollView
+				pagingEnabled
+				horizontal
+				showsHorizontalScrollIndicator={true}
+				contentContainerStyle={styles.weather}
+			>
+				<View style={styles.day}>
+					<Text style={styles.temp}>27</Text>
+					<Text style={styles.description}>Sunny</Text>
+				</View>
+				<View style={styles.day}>
+					<Text style={styles.temp}>27</Text>
+					<Text style={styles.description}>Sunny</Text>
+				</View>
+				<View style={styles.day}>
+					<Text style={styles.temp}>27</Text>
+					<Text style={styles.description}>Sunny</Text>
+				</View>
+			</ScrollView>
+		</View>
+	);
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+	container: {
+		flex: 1,
+		backgroundColor: 'black',
+	},
+	city: {
+		flex: 1.2,
+		justifyContent: 'center',
+		alignItems: 'center',
+	},
+	cityName: {
+		fontSize: 58,
+		fontWeight: '500',
+		color: 'white',
+	},
+	weather: {
+		// backgroundColor: 'blue',
+	},
+	day: {
+		width: SCREEN_WIDTH,
+		alignItems: 'center',
+	},
+	temp: {
+		marginTop: 50,
+		fontWeight: '600',
+		fontSize: 158,
+		color: 'white',
+	},
+	description: {
+		marginTop: -30,
+		fontSize: 50,
+		color: 'white',
+	},
 });
